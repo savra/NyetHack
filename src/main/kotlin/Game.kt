76fd581d5817e5889e300dee5fdb2218d6a5656a
Sergey Savrukhin <savra.sv@yandex.ru>
2023-02-26 17:03:53 +0300

    Завершил 4ую главу

--- src/main/kotlin/Game.kt
+++ src/main/kotlin/Game.kt
@@ -3,19 +3,42 @@ fun main(args: Array<String>) {
     var healthPoints = 89
     val isBlessed = true
     val isImmortal = false
-    val auraVisible = isBlessed && healthPoints > 50 || isImmortal
 
-    val auraColor = if (auraVisible) "GREEN" else "NONE"
+    val auraColor = auraColor(isBlessed, healthPoints, isImmortal)
 
-    val healthStatus = when (healthPoints) {
-        100 -> "is in excellent condition!"
-        in 90..99 -> "has a few scrathes."
-        in 75..89 -> if (isBlessed) "has some minor wounds but is healing quite quickly!" else "has some minor wounds."
-        in 15..74 -> "looks pretty hurt."
-        else -> "is in awful condition!"
-    }
+    val healthStatus = formatHealthStatus(healthPoints, isBlessed)
 
-    println("(Aura: $auraColor) " +
-            "(Blessed: ${if (isBlessed) "YES" else "NO"})")
+    printPlayerStatus(auraColor, isBlessed, name, healthStatus)
+
+    castFureball()
+}
+
+private fun castFureball(numFireballs: Int = 2) =
+    println("A glass of Fireball springs into existence. (x$numFireballs)")
+
+private fun printPlayerStatus(
+    auraColor: String,
+    isBlessed: Boolean,
+    name: String,
+    healthStatus: String
+) {
+    println(
+        "(Aura: $auraColor) " +
+                "(Blessed: ${if (isBlessed) "YES" else "NO"})"
+    )
     println("$name $healthStatus")
+}
+
+private fun auraColor(isBlessed: Boolean, healthPoints: Int, isImmortal: Boolean): String {
+    val auraVisible = isBlessed && healthPoints > 50 || isImmortal
+    val auraColor = if (auraVisible) "GREEN" else "NONE"
+    return auraColor
+}
+
+private fun formatHealthStatus(healthPoints: Int, isBlessed: Boolean) = when (healthPoints) {
+    100 -> "is in excellent condition!"
+    in 90..99 -> "has a few scrathes."
+    in 75..89 -> if (isBlessed) "has some minor wounds but is healing quite quickly!" else "has some minor wounds."
+    in 15..74 -> "looks pretty hurt."
+    else -> "is in awful condition!"
 }
