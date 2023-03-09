c52ff7d4fc9e1284cb81b76a008edc9551f2740d
Sergey Savrukhin <savra.sv@yandex.ru>
2023-03-09 18:37:07 +0300

    Завершил раздел про списки

--- src/main/kotlin/Tavern.kt
+++ src/main/kotlin/Tavern.kt
@@ -5,8 +5,29 @@ const val TAVERN_NAME = "Taernyl's Folly"
 var playerGold = 10
 var playerSilver = 10
 
+val patronList = mutableListOf("Eli", "Mordoc", "Sophie")
+val readOnlyPatronList = patronList.toList()
+
 fun main(args: Array<String>) {
+    if (patronList.contains("Eli")) {
+        println("The tavern master says: Eli's in the back playing cards.")
+    } else {
+        println("The tavern master says: Eli isn't here.")
+    }
+
+    if (patronList.containsAll(listOf("Sophie", "Mordoc"))) {
+        println("The tavern master says: Yea, they're seated by the stew kettle.")
+    } else {
+        println("The tavern master says: Nay, they departed hours ago.")
+    }
+
     placeOrder("shandy,Dragon's Breath,5.91")
+    println(patronList)
+    patronList.remove("Eli")
+    patronList.add("Alex")
+    patronList.add(0, "Alex")
+    patronList[0] = "Alexis"
+    println(patronList)
 }
 
 fun performPurchase(price: Double) {
